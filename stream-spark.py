bff46b9562f5d4878b995fc0b867ac9163cd9ce7
Diluc3004 <ragnvindrd041@gmail.com>
2025-03-21 18:34:11 -0600

    reapsldo

--- stream-spark.py
+++ stream-spark.py
@@ -26,3 +26,30 @@ def post_spark_job(user, repo, job, token):
 
     # Display the response in the app
     st.write(response)
+
+def get_spark_results(url_results):
+    response = requests.get(url_results)
+    st.write(response)
+
+    if  (response.status_code ==  200):
+        st.write(response.json())
+
+
+st.title("Spark & streamlit")
+
+st.header("spark-submit Job")
+
+github_user  =  st.text_input('Github user', value='JacquiVA02')
+github_repo  =  st.text_input('Github repo', value='evidenciaST')
+spark_job    =  st.text_input('Spark job', value='spark')
+github_token =  st.text_input('Github token', value='***')
+
+if st.button("POST spark submit"):
+    post_spark_job(github_user, github_repo, spark_job, github_token)
+
+st.header("spark-submit results")
+
+url_results=  st.text_input('URL results', value='https://raw.githubusercontent….')
+
+if st.button("GET spark results"):
+    get_spark_results(url_results)
